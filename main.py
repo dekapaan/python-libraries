83e09c69e37522a989943ce88e8f26579d65c2c6
dekapaan <terblancheronald@gmail.com>
2021-05-18 16:32:25 +0200

    sort out please

--- main.py
+++ main.py
@@ -1,48 +1,87 @@
-# Task 1
-# Number guessing game
-import random
-# random number to guess
-number = random.randint(1, 100)
+# # Task 1
+# # Number guessing game
+# import random
+# # random number to guess
+# number = random.randint(1, 100)
+#
+# # Gets number of guesses from user. If invalid entry given, gives user retry until valid entry given
+# while True:
+#     try:
+#         guesses = int(input('Enter number of guesses: '))
+#         break
+#     except ValueError:
+#         print('Not an integer, try again...')
+#
+# """Allows number of guesses stated by user. If guess incorrect, will tell user if guess too high or too low. If
+# invalid entry given, gives user retry until valid entry is given """
+# for x in range(guesses-1, -1, -1):
+#     while True:
+#         try:
+#             guess = int(input('Enter guess:'))
+#             break
+#         except ValueError:
+#             print("Not an integer")
+#     if guess == number:
+#         print('You win! :-)')
+#         break
+#     elif x == 0:
+#         print('All out of guesses. The number was {}. Better luck next time :-('.format(number))
+#     elif guess < number:
+#         print('Too low. {} guesses left'.format(x))
+#     elif guess > number:
+#         print('Too high. {} guesses left'.format(x))
 
-
-# Gets number of guesses from user. If invalid entry given, gives user retry until valid entry given
-while True:
-    try:
-        guesses = int(input('Enter number of guesses: '))
-        break
-    except ValueError:
-        print('Not an integer, try again...')
-
-
-"""Allows number of guesses stated by user. If guess incorrect, will tell user if guess too high or too low. If 
-invalid entry given, gives user retry until valid entry is given """
-for x in range(guesses-1, -1, -1):
-    while True:
-        try:
-            guess = int(input('Enter guess:'))
-            break
-        except ValueError:
-            print("Not an integer")
-    if guess == number:
-        print('You win! :-)')
-        break
-    elif x == 0:
-        print('All out of guesses. The number was {}. Better luck next time :-('.format(number))
-    elif guess < number:
-        print('Too low. {} guesses left'.format(x))
-    elif guess > number:
-        print('Too high. {} guesses left'.format(x))
-
-
-# Task 2
-# Ten dates from today, 2 weeks apart
+# # Exercise 1
+# # Ten dates from today, 1 week apart
 from datetime import datetime, timedelta
+#
+# # Today's date
+# dt = datetime.now()
+#
+# # Prints 10 dates 14 days apart
+# for x in range(10):
+#     print(x+1, "-", end='    ')
+#     print(dt.strftime('%Y/ %m/ %d - %H:%M'))
+#     dt = dt + timedelta(days=7)
 
-# Today's date
-dt = datetime.now()
+# Exercise 2
+# year_born = int(input('Enter year born:'))
+# month_born = int(input('Enter month born:'))
+# day_born = int(input('Enter day born:'))
+#
+# current_year = int(datetime.today().strftime('%Y'))
+# current_month = int(datetime.today().strftime('%m'))
+# current_day = int(datetime.today().strftime('%d'))
+#
+# age = current_year - year_born - 1
+#
+# if month_born < current_month:
+#     age += 1
+# elif month_born == current_month:
+#     if day_born <= current_day:
+#         age += 1
+#
+# print('')
+# print('You are {} years old'.format(age))
 
-# Prints 10 dates 14 days apart
-for x in range(9):
-    print(x+1, "-", end='    ')
-    print(dt.strftime('%Y/ %m/ %d'))
-    dt = dt + timedelta(days=14)
+# tkinter class area of circle
+from tkinter import *
+from math import pi
+
+
+class Circle(object):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def circle_area(self):
+        result = (pi*(self.radius**2))
+        return result
+
+
+root = Tk()
+root.geometry('700x500')
+root.config(bg='#555')
+
+
+
+root.mainloop()
